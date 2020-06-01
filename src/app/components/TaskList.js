e457677836beff7c910cf2506acc1512fe6c3d02
Dillon Coffman <tug40394@temple.edu>
2020-06-01 16:46:25 -0400

    Added Redux mutation for createTask and requestTaskCreation dispatch in TaskList; installed redux-saga and redux-logger to log out actions being dispatched

--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestTaskCreation } from '../store/mutations';
 
-export const TaskList = ({tasks, name}) => (
+export const TaskList = ({tasks, name, id, createNewTask}) => (
   <>
   <h3>{name}</h3>
   <div>
-    {tasks.map(task => (<div>{task.name}</div>))}
+    {tasks.map(task => (<div key={task.id}>{task.name}</div>))}
   </div>
+  <button type="button" onClick={() => createNewTask(id)}>Add New</button>
   </>
 );
 
@@ -19,4 +21,13 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask(id) {
+      console.log("Creating a new task...", id);
+      dispatch(requestTaskCreation(id));
+    }
+  };
+};
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
